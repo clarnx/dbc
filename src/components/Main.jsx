e91e2586b9dc097d67269abc5e4544b2d86a3451
0tilia <88244322+0tilia@users.noreply.github.com>
2023-12-04 11:22:49 +0200

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,10 +10,10 @@ export default function Main() {
             <h3 className="h3--main">Frontend Developer</h3>
             <h5 className="h5--main">www.github.com/0tilia</h5>
             <a href="https://www.linkedin.com/in/otilia-toma/">
-            <img src="src/images/linkedin--button.png" className="linkedin--button" />
+            <img src="/src/images/linkedin--button.png" className="linkedin--button" />
             </a>
             <a href="mailto:otiliatoma04@gmail.com" >
-            <img src="src/images/email--button.png" className="email--button" />
+            <img src="/src/images/email--button.png" className="email--button" />
             </a>
 
             </card>
